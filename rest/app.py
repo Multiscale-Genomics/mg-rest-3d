fad8ded1de4f39ea09b1932ada7a3b4b6b6d7522
Mark McDowall <mark.mcdowall@gmail.com>
2017-02-03 10:50:01 +0000

    Modified the URLs

--- rest/app.py
+++ rest/app.py
@@ -35,13 +35,13 @@ class GetEndPoints(Resource):
         return {
             '_links': {
                 '_self': request.base_url,
-                '_resolutions': request.url_root + 'api/3dcoord/resolutions',
-                '_chromosomes': request.url_root + 'api/3dcoord/chromosomes',
-                '_regions': request.url_root + 'api/3dcoord/regions',
-                '_models': request.url_root + 'api/3dcoord/models',
-                '_model': request.url_root + 'api/3dcoord/model',
-                '_ping': request.url_root + 'api/3dcoord/ping',
-                '_parent': request.url_root + 'api'
+                '_resolutions': request.url_root + 'mug/api/3dcoord/resolutions',
+                '_chromosomes': request.url_root + 'mug/api/3dcoord/chromosomes',
+                '_regions': request.url_root + 'mug/api/3dcoord/regions',
+                '_models': request.url_root + 'mug/api/3dcoord/models',
+                '_model': request.url_root + 'mug/api/3dcoord/model',
+                '_ping': request.url_root + 'mug/api/3dcoord/ping',
+                '_parent': request.url_root + 'mug/api'
             }
         }
 
@@ -56,7 +56,7 @@ class GetResolutions(Resource):
         usage = {
                     '_links' : {
                         '_self' : request.base_url,
-                        '_parent': request.url_root + 'api/3dcoord'
+                        '_parent': request.url_root + 'mug/api/3dcoord'
                     },
                     'parameters' : {
                         'user_id' : ['User ID', 'str', 'REQUIRED'],
@@ -105,7 +105,7 @@ class GetResolutions(Resource):
                 {
                     'resolution' : r,
                     '_links' : {
-                        '_chromosomes' : request.url_root + 'api/3dcoord/chromosomes?user_id=' + user_id + '&file_id=' + file_id + '&res=' + str(r)
+                        '_chromosomes' : request.url_root + 'mug/api/3dcoord/chromosomes?user_id=' + user_id + '&file_id=' + file_id + '&res=' + str(r)
                     }
                 }
             )
@@ -114,7 +114,7 @@ class GetResolutions(Resource):
         
         data['_links'] = {
             '_self': request.base_url + '?user_id=' + user_id + '&file_id=' + file_id,
-            '_parent': request.url_root + 'api/3dcoord'
+            '_parent': request.url_root + 'mug/api/3dcoord'
         }
         
         return data
@@ -130,7 +130,7 @@ class GetChromosomes(Resource):
         usage = {
                     '_links' : {
                         '_self' : request.base_url,
-                        '_parent': request.url_root + 'api/3dcoord'
+                        '_parent': request.url_root + 'mug/api/3dcoord'
                     },
                     'parameters' : {
                         'user_id' : ['User ID', 'str', 'REQUIRED'],
@@ -184,7 +184,7 @@ class GetChromosomes(Resource):
                 {
                     'chromosome' : c,
                     '_links' : {
-                        '_regions' : request.url_root + 'api/3dcoord/regions?user_id=' + user_id + '&file_id=' + file_id + '&res=' + str(resolution) + '&chr=' + str(c) + '&start=0&end=1000000000'
+                        '_regions' : request.url_root + 'mug/api/3dcoord/regions?user_id=' + user_id + '&file_id=' + file_id + '&res=' + str(resolution) + '&chr=' + str(c) + '&start=0&end=1000000000'
                     }
                 }
             )
@@ -194,8 +194,8 @@ class GetChromosomes(Resource):
         
         data['_links'] = {
             '_self': request.base_url + '?user_id=' + user_id + '&file_id=' + file_id + '&res=' + str(resolution),
-            '_parent': request.url_root + 'api/3dcoord',
-            '_resolution' : request.url_root + 'api/3dcoord/resolutions?user_id=' + user_id + '&file_id=' + file_id
+            '_parent': request.url_root + 'mug/api/3dcoord',
+            '_resolution' : request.url_root + 'mug/api/3dcoord/resolutions?user_id=' + user_id + '&file_id=' + file_id
         }
         
         return data
@@ -211,7 +211,7 @@ class GetRegions(Resource):
         usage = {
                     '_links' : {
                         '_self' : request.base_url,
-                        '_parent': request.url_root + 'api/3dcoord'
+                        '_parent': request.url_root + 'mug/api/3dcoord'
                     },
                     'parameters' : {
                         'user_id' : ['User ID', 'str', 'REQUIRED'],
@@ -272,7 +272,7 @@ class GetRegions(Resource):
                 {
                     'region_id' : r,
                     '_links' : {
-                        '_models' : request.url_root + 'api/3dcoord/models?user_id=' + user_id + '&file_id=' + file_id + '&res=' + str(resolution) + '&region=' + r
+                        '_models' : request.url_root + 'mug/api/3dcoord/models?user_id=' + user_id + '&file_id=' + file_id + '&res=' + str(resolution) + '&region=' + r
                     }
                 }
             )
@@ -283,9 +283,9 @@ class GetRegions(Resource):
         
         data['_links'] = {
             '_self': request.base_url + '?user_id=' + user_id + '&file_id=' + file_id + '&res=' + str(resolution) + '&chr=' + str(chr_id) + '&start=' + str(start) + '&end=' + str(end),
-            '_parent': request.url_root + 'api/3dcoord',
-            '_resolution' : request.url_root + 'api/3dcoord/resolutions?user_id=' + user_id + '&file_id=' + file_id,
-            '_chromosomes' : request.url_root + 'api/3dcoord/chromosomes?user_id=' + user_id + '&file_id=' + file_id + '&res=' + str(resolution)
+            '_parent': request.url_root + 'mug/api/3dcoord',
+            '_resolution' : request.url_root + 'mug/api/3dcoord/resolutions?user_id=' + user_id + '&file_id=' + file_id,
+            '_chromosomes' : request.url_root + 'mug/api/3dcoord/chromosomes?user_id=' + user_id + '&file_id=' + file_id + '&res=' + str(resolution)
         }
         
         return data
@@ -356,15 +356,15 @@ class GetModels(Resource):
                 'model' : str(m[0]),
                 'cluster' : str(m[1]),
                 '_links' : {
-                    '_model' : request.url_root + 'api/3dcoord/model?user_id=' + user_id + '&file_id=' + file_id + '&res=' + str(resolution) + '&region=' + str(region_id) + '&model=' + str(m[0])
+                    '_model' : request.url_root + 'mug/api/3dcoord/model?user_id=' + user_id + '&file_id=' + file_id + '&res=' + str(resolution) + '&region=' + str(region_id) + '&model=' + str(m[0])
                 }
             } for m in model_list
         ]
         
         models['_links'] = {
             '_self': request.base_url + '?user_id=' + user_id + '&file_id=' + file_id + '&res=' + str(resolution) + '&region=' + str(region_id),
-            '_parent': request.url_root + 'api/3dcoord',
-            '_models_all': request.url_root + 'api/3dcoord/model?user_id=' + user_id + '&file_id=' + file_id + '&res=' + str(resolution) + '&region=' + str(region_id) + '&model=all'
+            '_parent': request.url_root + 'mug/api/3dcoord',
+            '_models_all': request.url_root + 'mug/api/3dcoord/model?user_id=' + user_id + '&file_id=' + file_id + '&res=' + str(resolution) + '&region=' + str(region_id) + '&model=all'
         }
         
         current_region = region_list.index(region_id)
@@ -372,9 +372,9 @@ class GetModels(Resource):
         previous_region = current_region-1
         
         if current_region<(len(region_list)-1):
-            models['_links']['_next_region'] = request.url_root + 'api/3dcoord/models?user_id=' + user_id + '&file_id=' + file_id + '&res=' + str(resolution) + '&region=' + region_list[next_region]
+            models['_links']['_next_region'] = request.url_root + 'mug/api/3dcoord/models?user_id=' + user_id + '&file_id=' + file_id + '&res=' + str(resolution) + '&region=' + region_list[next_region]
         if current_region>0:
-            models['_links']['_previous_region'] = request.url_root + 'api/3dcoord/models?user_id=' + user_id + '&file_id=' + file_id + '&res=' + str(resolution) + '&region=' + region_list[previous_region]
+            models['_links']['_previous_region'] = request.url_root + 'mug/api/3dcoord/models?user_id=' + user_id + '&file_id=' + file_id + '&res=' + str(resolution) + '&region=' + region_list[previous_region]
         
         return models
 
@@ -458,7 +458,7 @@ class GetModel(Resource):
         
         models['_links'] = {
             '_self': request.base_url + '?user_id=' + user_id + '&file_id=' + file_id + '&res=' + str(resolution) + '&region=' + str(region_id) + '&model=' + str(model_str) + '&mpp=' + str(mpp) + '&page=' +str(page),
-            '_parent': request.url_root + 'api/3dcoord',
+            '_parent': request.url_root + 'mug/api/3dcoord',
         }
         
         models['query_data'] = {
@@ -469,9 +469,9 @@ class GetModel(Resource):
         }
         
         if (page) < model_meta['page_count']:
-             models['_links']['_next_page'] = request.url_root + 'api/3dcoord/model?user_id=' + user_id + '&file_id=' + file_id + '&res=' + str(resolution) + '&region=' + str(region_id) + '&model=' + str(model_str) + '&mpp=' + str(mpp) + '&page=' +str(page+1)
+             models['_links']['_next_page'] = request.url_root + 'mug/api/3dcoord/model?user_id=' + user_id + '&file_id=' + file_id + '&res=' + str(resolution) + '&region=' + str(region_id) + '&model=' + str(model_str) + '&mpp=' + str(mpp) + '&page=' +str(page+1)
         if (page) > 1:
-             models['_links']['_previous_page'] = request.url_root + 'api/3dcoord/model?user_id=' + user_id + '&file_id=' + file_id + '&res=' + str(resolution) + '&region=' + str(region_id) + '&model=' + str(model_str) + '&mpp=' + str(mpp) + '&page=' +str(page-1)
+             models['_links']['_previous_page'] = request.url_root + 'mug/api/3dcoord/model?user_id=' + user_id + '&file_id=' + file_id + '&res=' + str(resolution) + '&region=' + str(region_id) + '&model=' + str(model_str) + '&mpp=' + str(mpp) + '&page=' +str(page-1)
         
         return models
 
@@ -491,8 +491,8 @@ class ping(Resource):
             "name":    release.__rest_name__,
             "description": release.__description__,
             "_links" : {
-                '_self' : request.url_root + 'api/3dcoord/ping',
-                '_parent' : request.url_root + 'api/3dcoord'
+                '_self' : request.url_root + 'mug/api/3dcoord/ping',
+                '_parent' : request.url_root + 'mug/api/3dcoord'
             }
         }
         return res
@@ -501,20 +501,20 @@ class ping(Resource):
 Define the URIs and their matching methods
 """
 #   List the available end points for this service
-api.add_resource(GetEndPoints, "/api/3dcoord", endpoint='3dcoord_root')
+api.add_resource(GetEndPoints, "/mug/api/3dcoord", endpoint='3dcoord_root')
 
 #   Show the available resolutions
 #   Parameters:
 #    - file_id - (string)
 #    - user_id - (string)
-api.add_resource(GetResolutions, "/api/3dcoord/resolutions", endpoint='resolutions')
+api.add_resource(GetResolutions, "/mug/api/3dcoord/resolutions", endpoint='resolutions')
 
 #   Show the available chromosomes for a given resolution
 #   Parameters:
 #    - file_id - (string)
 #    - user_id - (string)
 #    - res     - resolution (int)
-api.add_resource(GetChromosomes, "/api/3dcoord/chromosomes", endpoint='chromosomes')
+api.add_resource(GetChromosomes, "/mug/api/3dcoord/chromosomes", endpoint='chromosomes')
 
 #   Show the available regions for a given chromosome, start, end and resolution
 #   Parameters:
@@ -524,14 +524,14 @@ api.add_resource(GetChromosomes, "/api/3dcoord/chromosomes", endpoint='chromosom
 #    - res     - resolution (int)
 #    - start   - chromosome start(int)
 #    - end     - chromosome end (int)
-api.add_resource(GetRegions, "/api/3dcoord/regions", endpoint='regions')
+api.add_resource(GetRegions, "/mug/api/3dcoord/regions", endpoint='regions')
 
 #   Show the available models for a given region_id
 #   Parameters:
 #    - file_id   - (string)
 #    - user_id   - (string)
 #    - region_id - region_id (int)
-api.add_resource(GetModels, "/api/3dcoord/models", endpoint='models')
+api.add_resource(GetModels, "/mug/api/3dcoord/models", endpoint='models')
 
 #   Show the 3D coordinates of a model for a given region_id
 #   Parameters:
@@ -539,10 +539,10 @@ api.add_resource(GetModels, "/api/3dcoord/models", endpoint='models')
 #    - user_id   - (string)
 #    - region_id - region_id (int)
 #    - model_id  - model_id (string)
-api.add_resource(GetModel, "/api/3dcoord/model", endpoint='model')
+api.add_resource(GetModel, "/mug/api/3dcoord/model", endpoint='model')
 
 #   Service ping
-api.add_resource(ping, "/api/3dcoord/ping", endpoint='adjacency-ping')
+api.add_resource(ping, "/mug/api/3dcoord/ping", endpoint='adjacency-ping')
 
 
 """
